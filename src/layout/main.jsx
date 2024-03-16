8daf0e3d37740176dbbd85d7e7b88df3564010be
Linell8 <elina_maslova_04@mail.ru>
2024-03-16 22:34:50 +0300

    replace http to https in fetch

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -9,14 +9,14 @@ class Main extends React.Component {
     loading: true,
     };
     componentDidMount () {
-    fetch('http://www.omdbapi.com/?apikey=cd4a6d46&s=matrix')
+    fetch('https://www.omdbapi.com/?apikey=cd4a6d46&s=matrix')
     .then ( (response) => response.json())
     .then ( (data) => this.setState ({ movies: data.Search, loading: false }));
     }
 
     searchMovies = (str, type = "all") => {
         fetch(
-            `http://www.omdbapi.com/?apikey=cd4a6d46&s=${str}${
+            `https://www.omdbapi.com/?apikey=cd4a6d46&s=${str}${
                 type !== "all" ? `&type=${type}` : ""
             }`
             )
